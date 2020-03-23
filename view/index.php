ded9308aa934c34896522c4aacb2c99bb21a76dd
Thomas <tg.59290@gmail.com>
2020-03-23 15:19:04 +0100

    add some indication on form validation to user in view

--- view/index.php
+++ view/index.php
@@ -6,9 +6,9 @@
   <h2>Add a new character to your adventure</h2>
   <!-- Action is specified to avoid remaining arguments in the url after clicking on clear -->
   <form action="index.php" method="post">
-    <label for="name">Your character's name :</label><br>
+    <label for="name">Your character's name (letters only) :</label><br>
     <input type="text" name="name" maxlength="50" pattern="[A-Za-z]+" required><br>
-    <label for="name">Age :</label><br>
+    <label for="name">Age (between 15 and 1000 years !) :</label><br>
     <input type="number" name="age" min="15" max="1000" required><br>
     <label for="name">Role type :</label><br>
     <select name="role" required>
@@ -16,8 +16,8 @@
       <option value="archer">Archer</option>
       <option value="wizard">Wizard</option>
     </select><br>
-    <label for="name">A short description :</label><br>
-    <textarea name="description" rows="8" cols="80" pattern="[A-Za-z]+" required></textarea>
+    <label for="name">A short description (letters only):</label><br>
+    <textarea name="description" rows="8" cols="80" maxlength="150" pattern="[A-Za-z]+" required></textarea>
     <br>
     <input type="submit" name="character_creation" value="Create">
   </form>
