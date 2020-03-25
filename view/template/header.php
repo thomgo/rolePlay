d0bd27c3f677429742dfe24bc81ca44c3e7778dc
Thomas <tg.59290@gmail.com>
2020-03-25 11:37:32 +0100

    add background image to header and center content

--- view/template/header.php
+++ view/template/header.php
@@ -5,10 +5,13 @@
     <title>The knight and the wizard</title>
     <link rel="stylesheet" href="public/css/normalize.css">
     <link rel="stylesheet" href="public/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|VT323&display=swap" rel="stylesheet">
   </head>
   <body>
     <header>
-      <h1>The knight and the wizard</h1>
-      <p>An epic adventure...</p>
+      <section>
+        <h1>The knight and the wizard</h1>
+        <p>An epic adventure...</p>
+      </section>
     </header>
     <main>
